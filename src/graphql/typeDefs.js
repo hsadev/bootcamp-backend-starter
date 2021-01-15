11c1534aff0b9ea45e2842ac3f36805c8c399a67
Lucia Lin <lucialin@college.harvard.edu>
2021-01-15 17:09:51 -0500

    wrote all types, inputs, and add mutations

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -4,6 +4,11 @@ module.exports = gql`
   type Mutation {
     login(email: String!, password: String!): AuthReturn!
     register(input: RegisterInput!): AuthReturn!
+    addUser(input: UserInput!):User!
+    addDiet(input: DietInput!):Diet!
+    addHealth(input: HealthInput!):Health!
+    addTriedRecipe(input: TriedRecipeInput!):TriedRecipe!
+    addBookmark(input: BookmarkInput!): Bookmark!
   }
 
   type Query {
@@ -13,8 +18,77 @@ module.exports = gql`
   type User {
     id: ID!
     email: String!
+    password: String!
+    firstName: String!
+    lastName: String!
     createdAt: String!
     updatedAt: String!
+  }
+
+  input UserInput {
+    email: String!
+    password: String!
+    firstName: String!
+    lastName: String!    
+  }
+
+  type Diet {
+    id: ID!
+    user: User!
+    restriction: String!
+  }
+
+  input DietInput {
+    user: UserInput!
+    restriction: String!    
+  }
+
+  type Health {
+    id: ID!
+    user: User!
+    restriction: String!
+  }
+
+  input HealthInput {
+    user: UserInput!
+    restriction: String!    
+  }
+
+  type TriedRecipe {
+    id: ID!
+    user: User!
+    link: String!
+  }
+
+  input TriedRecipeInput {
+    user: UserInput!
+    link: String!
+  }
+
+  type Bookmark {
+    id: ID!
+    user: User!
+    link: String!
+  }
+
+  input BookmarkInput {
+    user: UserInput!
+    link: String!    
+  }
+
+  type MealTracker {
+    id: ID!
+    user: User!
+    tried: TriedRecipe!
+    date: Date!
+    meal: String!
+  }
+
+  input MealTrackerInput {
+    user: UserInput!
+    tried: TriedRecipeInput!
+    date: Date!
+    meal: String!    
   }
 
   type AuthReturn {
@@ -26,4 +100,6 @@ module.exports = gql`
     email: String!
     password: String!
   }
+
+  scalar Date
 `
---
+++ src/lib/scalars.js
@@ -0,0 +1,6 @@
+const { GraphQLDate } = require('graphql-iso-date')
+
+const resolver = {
+  Date: GraphQLDate,
+}
+module.exports = resolver
