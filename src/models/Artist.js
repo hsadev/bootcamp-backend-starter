e74feaf7c1a6ae08f1a42d7dc5091823a6fc4969
Eric M Shen <eric.mm.shen@gmai.com>
2021-01-19 11:53:12 -0500

    artist model

---
+++ src/models/Artist.js
@@ -0,0 +1,25 @@
+const { BelongsToOneRelation, HasManyRelation } = require('objection')
+const BaseModel = require('./BaseModel')
+
+class Artist extends BaseModel {
+    static get tableName() {
+      return 'artists'
+    }
+  
+    static get relationMappings() {
+      const Song = require('./Address')
+  
+      return {
+        songs: {
+          relation: HasManyRelation,
+          modelClass: Song,
+          join: {
+            from: 'authors.id',
+            to: 'songs.artistId',
+          },
+        },
+      }
+    }
+  }
+  
+  module.exports = Artist
