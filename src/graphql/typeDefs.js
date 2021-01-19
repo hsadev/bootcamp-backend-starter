b6554fd22ef17ee3c4df820bdabbc3531567374e
mabog3 <63216393+mabog3@users.noreply.github.com>
2021-01-19 15:06:38 -0500

    basic typedefs

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -7,7 +7,21 @@ module.exports = gql`
   }
 
   type Query {
-    welcome: String!
+    allUsers: [User!]!
+    userById(id: ID!): User!
+    allCategories: [Category!]!
+    categoryById(id: ID!): Category!
+    allQuestions: [Question!]!
+    questionById(id: ID!): Question!
+    allScores: [Score!]!
+    scoresById(id: ID!): Score!
+  }
+
+  type Mutation {
+    addUser(input: userInput!): User!
+    addQuestion(input: questionInput): Question!
+    addScore(input: scoreInput): Score!
+    addCategory(input: categoryInput!): Category!
   }
 
   type User {
@@ -15,6 +29,71 @@ module.exports = gql`
     email: String!
     createdAt: String!
     updatedAt: String!
+    username: String!
+    password: String!
+    firstName: String!
+    lastName: String!
+    accuracy: Float
+    gamesPlayed: Int 
+  }
+
+  input userInput {
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+    username: String!
+    password: String!
+    firstName: String!
+    lastName: String! 
+  }
+
+  type Category {
+    id: ID! 
+    title: String!
+    description: String!
+    type: String! 
+    popularity: Float
+    difficulty: Float! 
+  }
+
+  input categoryInput {
+    title: String!
+    description: String!
+    type: String! 
+    difficulty: Float!
+  }
+
+  type Question {
+    id: ID!
+    categoryId: ID!
+    question: String!
+    answerMinimum: Int!
+    answerMaximum: Int!
+    popularity: Float 
+    difficulty: Float! 
+  }
+
+  type questionInput {
+    categoryId: ID!
+    question: String!
+    answerMinimum: Int!
+    answerMaximum: Int!
+    difficulty: Float! 
+  }
+
+  type Score {
+    id: ID!
+    userId: ID!
+    categoryId: ID!
+    score: Float!
+    timeScored: String!
+  }
+
+  input scoreInput {
+    userId: ID!
+    categoryId: ID!
+    score: Float!
+    timeScored: String!
   }
 
   type AuthReturn {
@@ -27,3 +106,4 @@ module.exports = gql`
     password: String!
   }
 `
+// need to make category type enum, not String
