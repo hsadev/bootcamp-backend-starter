71b7823583701723e491dd25e16a3ae3d32241b8
Alex Estevez <aestevez@college.harvard.edu>
2021-01-19 14:54:36 -0500

    added typedefs, queries for users and courses (untested)

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,15 +1,18 @@
 const { gql } = require('apollo-server-express')
-const typeDefs = require('../../../bootcamp-backend-demo/src/graphql/typeDefs')
 
 module.exports = gql`
   type Mutation {
     login(email: String!, password: String!): AuthReturn!
     register(input: RegisterInput!): AuthReturn!
-
+    addCourse(course: addCourseInput!): Course
+    
   }
-
   type Query {
-    welcome: String!
+    userCourses(userID: ID!): [Course!]
+    userById(userID: ID!): User
+    userByEmail(email: String!): User
+    courseById(courseID: ID!): Course
+
   }
 
   type User {
@@ -20,6 +23,7 @@ module.exports = gql`
     password: String!
     createdAt: String!
     updatedAt: String!
+    courses: [Course]
   }
 
   type Course {
@@ -39,6 +43,21 @@ module.exports = gql`
     user: User!
   }
 
+  input addCourseInput {
+    title: String!
+    professor: String!
+    monday: Boolean!
+    tuesday: Boolean!
+    wednesday: Boolean!
+    thursday: Boolean!
+    friday: Boolean!
+    timeStart: String
+    timeEnd: String
+    hoursPerWeek: Int
+    description: String
+    enrolled: Boolean!
+  }
+
   type AuthReturn {
     token: String!
     user: User!
@@ -49,5 +68,3 @@ module.exports = gql`
     password: String!
   }
 `
-
-module.exports = typeDefs
