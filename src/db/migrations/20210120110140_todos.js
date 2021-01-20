7d6a3d8d8eeeef832da30a5c88aa234e9544dd65
seanyboy65 <spmccabe@college.harvard.edu>
2021-01-20 11:16:28 -0500

    created migrations for todo list

--- src/db/migrations/20210120110140_todos.js
+++ src/db/migrations/20210120110140_todos.js
@@ -1,8 +1,21 @@
 
-exports.up = function(knex) {
+exports.up = async knex => createTableIfNotExists(knex, 'courses', table => {
+    table
+      .uuid('id')
+      .notNullable()
+      .primary()
+      .defaultTo(knex.raw('uuid_generate_v4()'))
   
-};
+    table
+      .string('name')
+      .notNullable()
+  
+    table
+      .text('description')
+    
+    table
+      .uuid('userId')
+      .references('users.id')
+  })
 
-exports.down = function(knex) {
-  
-};
+exports.down = async knex => knex.schema.dropTableIfExists('todos')
